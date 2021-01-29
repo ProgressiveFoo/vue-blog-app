036f921e1860a4d5ab0f269da9e8e4c51fdb50ce
Miroslav Pantos <bboypamik@gmail.com>
2021-01-29 19:30:52 +0100

    get all posts

---
+++ src/views/AppPosts.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+      <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">ID</th>
+            <th scope="col">title</th>
+            <th scope="col">text</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="post in posts" :key="post.id">
+							<td>{{post.id}}</td>
+							<td>{{post.title}}</td>
+							<td>{{post.text}}</td>
+            </tr>
+        </tbody>
+        </table>
+  </div>
+
+</template>
+
+<script>
+import PostService from '../services/PostService'
+export default {
+
+  data(){
+    return{
+      posts: []
+    }
+  },
+
+  async created() {
+     this.posts = await PostService.getAll()
+  },
+
+  mounted(){
+
+  }
+}
+</script>
+
+<style>
+
+</style>
